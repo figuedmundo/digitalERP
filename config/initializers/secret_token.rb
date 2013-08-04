f11ae183ab2f98aee9858d71b28635e0c70acced
Edmundo Figueroa Herbas <figuedmundo@gmail.com>
2013-08-04 19:40:37 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DigitalERP::Application.config.secret_token = 'a9b0cc6cb1a6a74b1d2d92ba5f680dbcea9a15f2a0a5ddc3e72d9daa0242c5bfe020a8799e3631d8f75756d7a502f3864aac689e2c6ecb2f280d1002525ccdf5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+DigitalERP::Application.config.session_store :cookie_store, key: '_digitalERP_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# DigitalERP::Application.config.session_store :active_record_store
